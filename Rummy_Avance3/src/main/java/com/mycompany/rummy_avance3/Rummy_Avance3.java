7f4e4d39667964faf49274faea36c56fe9d8cb36
Carlos H Sánchez <155609143+hiiramsan@users.noreply.github.com>
2024-09-23 22:29:40 -0700

    wrv
    ewc

---
+++ Rummy_Avance3/src/main/java/com/mycompany/rummy_avance3/Rummy_Avance3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rummy_avance3;
+
+/**
+ *
+ * @author Carlos Sanchez
+ */
+public class Rummy_Avance3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
