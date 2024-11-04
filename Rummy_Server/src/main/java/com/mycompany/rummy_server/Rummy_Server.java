b9571828a1e923447a3f0d8d59f052f5def177a0
777Styx <jullianpuerta@gmail.com>
2024-11-03 21:33:21 -0800

    servidor
    aun no se codifica

---
+++ Rummy_Server/src/main/java/com/mycompany/rummy_server/Rummy_Server.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rummy_server;
+
+/**
+ *
+ * @author julli
+ */
+public class Rummy_Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
