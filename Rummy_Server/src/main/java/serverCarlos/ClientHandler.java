c7fe65f866c469bd4f3bb7d22c09905b7b217f2e
Carlos H Sánchez <carloshiramsanchez@gmail.com>
2024-11-06 16:24:51 -0700

    Socket prueba

--- Rummy_Server/src/main/java/serverCarlos/ClientHandler.java
+++ Rummy_Server/src/main/java/serverCarlos/ClientHandler.java
@@ -4,27 +4,83 @@
  */
 package serverCarlos;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  *
  * @author carlo
  */
 public class ClientHandler implements Runnable {
-    
-    private final Socket socketClient;
-    private final Server servidor;
+    private static final ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private final Socket socket;
+    private final BufferedReader bufferedReader;
+    private final BufferedWriter bufferedWriter;
+    private final String clientUsername;
 
-    public ClientHandler(Socket socketClient, Server servidor) {
-        this.socketClient = socketClient;
-        this.servidor = servidor;
+    public ClientHandler(Socket socket) throws IOException {
+        this.socket = socket;
+        this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.clientUsername = bufferedReader.readLine();
+        clientHandlers.add(this);
+        broadcastMessage("SERVER " + clientUsername + " has entered");
     }
 
     @Override
     public void run() {
-        
+        String messageFromClient;
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                if (messageFromClient != null) {
+                    broadcastMessage(messageFromClient);
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
     }
-    
-    
-    
+
+    public void broadcastMessage(String messageToSend) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                if (clientHandler != this) {
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        System.out.println(clientUsername + " se salió del juego");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        removeClientHandler();
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Rummy_Server/src/main/java/serverCarlos/Server.java
+++ Rummy_Server/src/main/java/serverCarlos/Server.java
@@ -17,65 +17,56 @@ import servidor.Servidor;
  * @author carlo
  */
 public class Server {
-
     private static final int MAX_JUGADORES = 4;
-    private final Set<Socket> jugadoresConectados = Collections.synchronizedSet(new HashSet<>());
-    private final int puerto;
+    private final Set<ClientHandler> jugadoresConectados = Collections.synchronizedSet(new HashSet<>());
     private ServerSocket servidorSocket;
 
-    public Server(int puerto) {
-        this.puerto = puerto;
-        this.servidorSocket = servidorSocket;
+    public Server(ServerSocket serverSocket) {
+        this.servidorSocket = serverSocket;
     }
-    
+
     public void iniciar() {
         try {
-            while(!servidorSocket.isClosed()) {
+            while (!servidorSocket.isClosed()) {
                 Socket socket = servidorSocket.accept();
                 System.out.println("Un nuevo jugador se ha conectado!");
-                ClientHandler jugador = new ClientHandler(socket);
+
                 
-                Thread thread = new Thread(jugador);
-                thread.start();
+                    ClientHandler jugador = new ClientHandler(socket);
+                    jugadoresConectados.add(jugador);
+                    Thread thread = new Thread(jugador);
+                    thread.start();
+                
             }
-        } catch(IOException e) {
-        
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    
+
     public void detener() {
         try {
-            if(servidorSocket != null) {
+            if (servidorSocket != null) {
                 servidorSocket.close();
             }
             System.out.println("Servidor detenido.");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        
     }
-    
-    public Set<Socket> getJugadoresConectados() {
+
+    public Set<ClientHandler> getJugadoresConectados() {
         return jugadoresConectados;
     }
 
-
-    public void removerJugador(Socket jugador) {
+    public void removerJugador(ClientHandler jugador) {
         jugadoresConectados.remove(jugador);
         System.out.println("Jugador desconectado. Jugadores activos: " + jugadoresConectados.size());
     }
-    
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) throws IOException {
-        // TODO code application logic 
-        int puerto = 3030;
-        Server servidor = new Server(puerto);
+        ServerSocket serverSocket = new ServerSocket(2020);
+        Server servidor = new Server(serverSocket);
         servidor.iniciar(); // Inicia el servidor
         System.out.println("Server running...");
-        
     }
-    
-}   
+}
